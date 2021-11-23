f7044312614413d13377be985c9dd6b988c83712
Chris McVickar <mcvi1652@vandals.uidaho.edu>
2021-11-22 22:50:14 -0800

    Moving to pointers

--- Drum-MIDI-Converter/test/Node.h
+++ Drum-MIDI-Converter/test/Node.h
@@ -10,24 +10,25 @@ class Node {
 private:
     const std::string _key;
     const bool _isLeaf;
-    std::map<std::string, Node> _children;
+    const Node* _parentPtr;
+    std::map<std::string, Node*> _children;
     std::vector<std::string> _defaults;
 
     void print(const uint32_t&) const;
 public:
-    Node();
-    Node(const Node&);
-    Node(const std::string&);
-    Node(const std::string&, const std::vector<std::string>&, const std::vector<std::string>&);
-    Node(const std::string&, const bool&);
-    Node(const std::string&, const std::vector<Node>&, const std::vector<std::string>&);
-    bool isLeaf() const { return _isLeaf; };
-    std::string key() const { return _key; };
-    Node operator[](const std::string&);
-    Node operator[](const std::string&) const;
-    void addChild(const Node&);
-    void addDefault(const std::string&);
-    void print() const;
+    // Node();
+    // Node(const Node&);
+    // Node(const std::string&);
+    // Node(const std::string&, const std::vector<std::string>&, const std::vector<std::string>&);
+    // Node(const std::string&, const bool&);
+    // Node(const std::string&, const std::vector<Node>&, const std::vector<std::string>&);
+    // bool isLeaf() const { return _isLeaf; };
+    // std::string key() const { return _key; };
+    // Node operator[](const std::string&);
+    // Node operator[](const std::string&) const;
+    // void addChild(const Node&);
+    // void addDefault(const std::string&);
+    // void print() const;
 };
 
 #endif
