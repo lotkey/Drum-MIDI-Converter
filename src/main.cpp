169a489ff5252beafcc032413b9826a4c58eeb2d
Chris McVickar <mcvi1652@vandals.uidaho.edu>
2021-12-22 17:50:09 -0800

    Organized repo

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
-#include <string>
+#include "mainwindow.h"
 
-#include "./Mapping/ConversionMap.hpp"
-#include "./Mapping/Mapping.hpp"
-#include "./Mapping/Mappings.hpp"
-#include "./Parsing/ParseTree.hpp"
+#include <QCoreApplication>
+#include <QApplication>
 
-int main() {
-    ParseTree tree("./tree/");
-    std::map<std::string, Mapping> mappings = Mappings::getAllMappings();
-    ConversionMap m3 = tree.makeConversionMapping(mappings["GGD Modern & Massive"], mappings["Drumforge Bergstrand"]);
-    m3.print();
-
-    return 0;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setApplicationName("Drum MIDI Converter");
+    a.setOrganizationName("Lotkey Audio");
+    MainWindow w;
+    w.setWindowTitle("Drum MIDI Converter");
+    w.show();
+    return a.exec();
 }
