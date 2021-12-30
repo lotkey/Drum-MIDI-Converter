772493751f27c50f3ca66dad2bd3cfdeb8c40718
synthchris666 <63672369+lotkey@users.noreply.github.com>
2021-12-30 00:19:04 -0800

    added python app source

---
+++ src-python/app.py
@@ -0,0 +1,162 @@
+import dash
+from dash import dcc, html, Input, Output, State
+from conversionmap import ConversionMap
+import base64
+from mido import MidiFile
+
+DOWNLOAD_PATH = './upload.mid'
+CONVERTED_PATH = './converted.mid'
+CONVERSIONS_PATH = './conversions.lkcmap'
+
+app = dash.Dash(__name__)
+app.title = 'Drum MIDI Converter'
+server = app.server
+conversionMaps = ConversionMap.loadAllConversionMaps(CONVERSIONS_PATH)
+mappingNames = list(ConversionMap.getMappingNames(CONVERSIONS_PATH))
+mappingNames.sort()
+
+app.layout = html.Div(
+    id = 'parent', 
+    children = [
+        html.H1(
+            id = 'H1', 
+            children = 'Drum MIDI Converter', 
+            style = {
+                'textAlign':'left',
+                'margin':10,
+                'marginBottom':40
+            }
+        ),
+
+        dcc.Upload(
+            id = 'upload_fileInput',
+            children = [
+                html.Button(
+                    'Upload MIDI',
+                    id = 'button_upload'
+                ),
+                html.Div(
+                    children = '',
+                    id = 'filename'
+                )
+            ],
+            accept = '.mid',
+            style = {
+                'margin':10
+            }
+        ),
+
+        html.Div(
+            children = 'Convert from:',
+            style = {
+                'marginLeft':10
+            }
+        ),
+
+        dcc.Dropdown(
+            id = 'dropdown_mapIn',
+            options = [{'label':i, 'value':i.upper()} for i in mappingNames],
+            value = mappingNames[0].upper(),
+            style = {
+                'margin':10,
+                'marginLeft':5
+            }
+        ),
+
+        html.Div(
+            children = 'Convert to:',
+            style = {
+                'marginLeft':10
+            }
+        ),
+
+        dcc.Dropdown(
+            id = 'dropdown_mapOut',
+            options = [{'label':i, 'value':i.upper()} for i in mappingNames],
+            value = mappingNames[1].upper(),
+            style = {
+                'margin':10,
+                'marginLeft':5
+            }
+        ),
+
+        html.Button(
+            'Download MIDI',
+            id = 'download_fileOutput',
+            style = {
+                'marginLeft':10
+            },
+            disabled = True
+        ),
+
+        dcc.Download(id='download'),
+
+        html.Div(
+            id = 'hidden1',
+            style = {
+                'display':'none'
+            }
+        )
+    ]
+)
+
+@app.callback(
+    Output('download', 'data'),
+    Input('download_fileOutput', 'n_clicks'),
+    State('upload_fileInput', 'contents'),
+    State('upload_fileInput', 'filename'),
+    State('dropdown_mapIn', 'value'),
+    State('dropdown_mapOut', 'value'),
+    prevent_initial_call = True
+)
+def export(num_clicks, content, filename, mapIn, mapOut):
+    try:
+        save_file(DOWNLOAD_PATH, content)
+        map = conversionMaps[mapIn.upper()][mapOut.upper()]
+        convert_midi(map)
+
+        fn = f'''{filename[:filename.rfind('.')]} ({mapOut}).mid'''
+        with open(CONVERTED_PATH, 'rb') as infile:
+            return dcc.send_bytes(infile.read(), filename=fn)
+    except Exception as e:
+        return dict(content=str(e), filename='error.txt')
+
+@app.callback(
+    Output('download_fileOutput', 'disabled'),
+    Input('upload_fileInput', 'contents'),
+)
+def enable_download_button(contents):
+    if contents is not None:
+        return False
+    return dash.no_update
+
+@app.callback(
+    Output('filename', 'children'),
+    Input('upload_fileInput', 'filename')
+)
+def display_filename(filename):
+    return filename
+
+def save_file(path, content):
+    data = content.encode('utf8').split(b';base64,')[1]
+    with open(path, 'wb') as outfile:
+        outfile.write(base64.decodebytes(data))
+
+def convert_midi(conversionMap:ConversionMap):
+    inmidi = MidiFile(DOWNLOAD_PATH)
+    outmidi = MidiFile()
+
+    for i, track in enumerate(inmidi.tracks):
+        outmidi.add_track()
+        for j, msg in enumerate(track):
+            if (msg.type in ['note_on', 'note_off']):
+                if (conversionMap.contains(msg.note)):
+                    msg.note = conversionMap.at(msg.note)
+                    outmidi.tracks[i].append(msg)
+            else:
+                outmidi.tracks[i].append(msg)
+
+    outmidi.save(file=open(CONVERTED_PATH, 'wb'))
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ src-python/conversionmap.py
@@ -0,0 +1,109 @@
+import os
+
+class ConversionMap():
+    mappingNames = set()
+
+    def loadAllConversionMaps(path:str):
+        addingNames = len(ConversionMap.mappingNames) == 0
+        mappings = dict()
+        if not os.path.isdir(path):
+            with open(path, 'r') as infile:
+                lines = [line.strip() for line in infile.readlines()]
+                lookingForMapFrom = lookingForMapTo = True
+                appendNext = False
+
+                for line in lines:
+                    if lookingForMapFrom:
+                        mapFrom = line
+                        if addingNames:
+                            ConversionMap.mappingNames.add(mapFrom)
+                        lookingForMapFrom = False
+                    elif lookingForMapTo:
+                        mapTo = line
+                        lookingForMapTo = False
+                        appendNext = True
+                    elif line == "//":
+                        lookingForMapFrom = lookingForMapTo = True
+                        appendNext = False
+                    else:
+                        if appendNext:
+                            if mapFrom.upper() in mappings:
+                                mappings[mapFrom.upper()][mapTo.upper()] = ConversionMap(mapFrom=mapFrom, mapTo=mapTo)
+                            else:
+                                mappings[mapFrom.upper()] = dict()
+                                mappings[mapFrom.upper()][mapTo.upper()] = ConversionMap(mapFrom=mapFrom, mapTo=mapTo)
+                            appendNext = False
+                        
+                        pair = line.split()
+                        if len(pair) == 2:
+                            mappings[mapFrom.upper()][mapTo.upper()].insert((int(pair[0]), int(pair[1])))
+
+
+                
+        else:
+            for (dirpath, _, filenames) in os.walk(path):        
+                for filename in filenames:
+                    map = ConversionMap(f'{dirpath}/{filename}')
+                    map1 = map.mapFrom.upper()
+                    map2 = map.mapTo.upper()
+                    if map1 not in mappings:
+                        mappings[map1] = dict()
+                    mappings[map1][map2] = map
+        return mappings
+
+    def getMappingNames(path:str):
+        if len(ConversionMap.mappingNames) != 0:
+            return ConversionMap.mappingNames
+        else:
+            if os.path.isdir(path):
+                return os.listdir(path)
+            else:
+                names = set()
+                with open(path, 'r') as infile:
+                    lines = infile.readlines()
+                lines = [line.strip() for line in lines]
+                linesSplit = [line.split() for line in lines]
+                isDigits = [[j.isdecimal() or j == '//' for j in i] for i in linesSplit]
+                isDigits = [all(i) for i in isDigits]
+
+                for i, line in enumerate(lines):
+                    if not isDigits[i]:
+                        names.add(line)
+                ConversionMap.mappingNames = names
+                return ConversionMap.mappingNames
+
+
+    def __init__(self, path:str=None, mapFrom:str=None, mapTo:str=None):
+        self.map = dict()
+
+        if path is None:
+            assert mapFrom is not None or mapTo is not None
+            self.mapFrom = mapFrom
+            self.mapTo = mapTo
+        else:
+            with open(path, 'r') as infile:
+                lines = infile.readlines()
+
+                self.mapFrom = lines[0].strip()
+                lines.pop(0)
+                self.mapTo = lines[0].strip()
+                lines.pop(0)
+                
+                for line in lines:
+                    mapping = line.split()
+                    self.map[int(mapping[0])] = int(mapping[1])
+
+    def print(self):
+        print(f'Conversion from "{self.mapFrom}" to "{self.mapTo}"')
+
+        for key in self.map:
+            print(f'{key} => {self.map[key]}')
+
+    def insert(self, pair:tuple):
+        self.map[tuple[0]] = tuple[1]
+
+    def at(self, key:int):
+        return self.map[key]
+
+    def contains(self, key:int):
+        return key in self.map
